2f2dce1a9f603ba81611276b89a99726cb6c1cc6
SaadNasir-Drago <snasir2194@gmail.com>
2024-12-20 23:42:20 +0600

    2nd completed

--- src/app/user-agent/page.tsx
+++ src/app/user-agent/page.tsx
@@ -1,7 +1,10 @@
 import { UserAgent } from "@/views/userAgent";
+import { headers } from "next/headers";
 
-const UserAgentRoot = () => {
-  return <UserAgent />;
+const UserAgentRoot = async () => {
+  // Fetch the User-Agent from server headers
+  const userAgent = (await headers()).get("user-agent") || "Unknown User Agent";
+  return <UserAgent userAgent={userAgent} />;
 };
 
 export default UserAgentRoot;
